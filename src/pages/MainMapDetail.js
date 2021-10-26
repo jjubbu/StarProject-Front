8f007b68b96f2356dee8d639812f32fc7d3cd016
JiyeonLee <sdjfbr@naver.com>
2021-10-26 21:35:34 +0900

    수정추가

---
+++ src/pages/MainMapDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Post from "../components/MainMap";
+
+const PostDetail = (props) => {
+  return (
+    <React.Fragment>
+      <MainMap />
+    </React.Fragment>
+  );
+};
+
+export default PostDetail;
