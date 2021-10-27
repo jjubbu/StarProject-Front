814b6d5f7322b9571e0aad87e885fef7a5a6d5b4
jjubbu <hogugugut4102.off@gmail.com>
2021-10-27 14:02:57 +0900

    Style login, signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { apis } from "../lib/axios";
 import styled from "styled-components";
+import {Cookies} from "react-cookie";
+
+import { history } from "../redux/configureStore";
 
 const Login = () => {
 
@@ -15,13 +18,18 @@ const Login = () => {
         console.log(loginInfo)
         apis.loginAX(loginInfo).then((response)=>{
             console.log(response);
+            // if(response.data.code === 200){
+            //     alert('로그인 성공!');
+
+
+            // }
         })
     }
       
 
     return (
         <React.Fragment>
-            <article>
+            <StyleArticle>
                 <h1>로그인</h1>
                 <section>
                 <label>
@@ -34,10 +42,37 @@ const Login = () => {
                 </label>
                 </section>
                 <button onClick={login}>로그인</button>
-                <button>회원가입</button>
-            </article>
+                <button onClick={()=>{history.push('/signup')}}>회원가입</button>
+            </StyleArticle>
         </React.Fragment>
     )
 }
+
+const StyleArticle = styled.article`
+display: flex;
+flex-direction: column;
+margin: 0 auto;
+width:50%;
+height: 100%;
+max-width: 780px;
+gap:10px;
+
+justify-content: center;
+align-items: center;
+section{
+    width:100%;
+    label{
+        display: block;
+        width: 100%;
+        margin: 20px 0;
+        input{
+            width:100%;
+        }
+    }
+}
+button{width:100%; padding:10px;}
+
+
+`
 
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { apis } from "../lib/axios";
+import styled from "styled-components";
 
 const Signup = () => {
 
@@ -18,6 +19,7 @@ const Signup = () => {
     }
     return (
         <React.Fragment>
+            <StyleArticle>
             <h1>회원가입</h1>
             <section>
             <label>
@@ -40,8 +42,37 @@ const Signup = () => {
             </label>
             </section>
             <button onClick={signup}>회원가입</button>
+            </StyleArticle>
         </React.Fragment>
     )
 }
+
+
+const StyleArticle = styled.article`
+display: flex;
+flex-direction: column;
+margin: 0 auto;
+width:50%;
+height: 100%;
+max-width: 780px;
+gap:10px;
+
+justify-content: center;
+align-items: center;
+section{
+    width:100%;
+    label{
+        display: block;
+        width: 100%;
+        margin: 20px 0;
+        input{
+            width:100%;
+        }
+    }
+}
+&>button{width:100%; padding:10px;}
+
+
+`
 
 export default Signup;
