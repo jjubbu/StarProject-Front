ec7cb377dc479a192cc142e90fdd5055a10bd241
JiyeonLee <sdjfbr@naver.com>
2021-11-23 13:03:07 +0900

    Edit

--- src/components/SimpleSlider.js
+++ src/components/SimpleSlider.js
@@ -5,34 +5,39 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-const SimpleSlider = () => {
-  const settings = {
-    dots: false, // 점 안보임
-    infinite: true, // 무한넘김
-    speed: 500,
-    slidesToShow: 4, // 4장씩 보이게
-    slidesToScroll: 1, //1장씩 넘어감
-  };
-
-  return (
-    <Slider {...settings}>
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+    return (
       <div>
-        <h3>1</h3>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
       </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-    </Slider>
-  );
-};
-
-export default SimpleSlider;
+    );
+  }
+}
