57a8ad798271ec1c2fea479f4be84e27f91f23ce
jjubbu <hogugugut4102.off@gmail.com>
2021-11-29 18:56:19 +0900

    Edit: 프리렌더링 추가

--- src/index.js
+++ src/index.js
@@ -6,15 +6,29 @@ import { Provider } from "react-redux";
 import store from "./redux/configureStore";
 import { GlobalStyle } from "./shared/global";
 import { BrowserRouter } from "react-router-dom";
+import { render, hydrate } from "@testing-library/react";
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <Provider store={store}>
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <Provider store={store}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootElement
+  );
+} else {
+  render(
+    <Provider store={store}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootElement
+  );
+}
 
 reportWebVitals();
