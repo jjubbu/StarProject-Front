e5a583b77427a33eb99e1569b6c69d79e9c8c3ff
jjubbu <hogugugut4102.off@gmail.com>
2021-12-01 10:52:10 +0900

    Edit: 주석 제거

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -2,10 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Grid = (props) => {
-  // 상위 컴포넌트로부터 Props전달받기
   const { is_flex, width, margin, padding, bg, children, center } = props;
 
-  // 전달받은 props를 별도의 변수에 저장
   const styles = {
     is_flex: is_flex,
     width: width,
@@ -17,19 +15,10 @@ const Grid = (props) => {
 
   return (
     <React.Fragment>
-      {/* 위에서 선언한 styles를 {...styles}로 적용해줍니다 */}
-      {/* props로 가져온 값들은 따로 아래와 같이 inline으로 먹여줘야함 */}
-      <GridBox {...styles}>
-        {children}
-        {/*{children}또는 props.children을 미리 넣어줘야한다. 안그러면 이 안에 다른 
-                컴포넌트 넣었을 때 안보임 */}
-      </GridBox>
+      <GridBox {...styles}>{children}</GridBox>
     </React.Fragment>
   );
 };
-
-// Grid의 default Props
-// 부여할 것이라고 예상되는 스타일들에 대해 미리 값을 부여해놓는다
 Grid.defaultProps = {
   children: null,
   is_flex: false,
@@ -39,8 +28,6 @@ Grid.defaultProps = {
   bg: false,
   center: false,
 };
-
-// styled components
 const GridBox = styled.div`
   width: ${(props) => props.width};
   height: 100%;
